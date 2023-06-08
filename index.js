0f24a0339a762a53dbb2fb973423b3709f585d69
AbdullahWB <abdullah917828@gmail.com>
2023-06-08 09:32:28 +0600

    put user in db

--- index.js
+++ index.js
@@ -28,6 +28,22 @@ async function run() {
     try {
         const usersCollection = client.db('MartialArtsQuest').collection('users')
 
+
+
+        app.put('/users/:email', async (req, res) => {
+            const email = req.params.email
+            const user = req.body
+            const query = { email: email }
+            const options = { upsert: true }
+            const updateDoc = {
+                $set: user,
+            }
+            const result = await usersCollection.updateOne(query, updateDoc, options)
+            console.log(result);
+            res.send(result)
+        })
+
+
         // Send a ping to confirm a successful connection
         await client.db('admin').command({ ping: 1 })
         console.log(
